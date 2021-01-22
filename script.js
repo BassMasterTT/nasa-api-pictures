e26352eca34c1776f796ef9c9b207bdbe7a57fae
BassMasterTT <tothtibi22@gmail.com>
2021-01-22 19:21:05 +0800

    save favorites.js

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const apiKey = "DEMO_KEY";
 const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${count}`;
 
 let resultsArray = [];
+let favorites = {};
 
 function updateDOM() {
   resultsArray.forEach((result) => {
@@ -38,6 +39,7 @@ function updateDOM() {
     const saveText = document.createElement("p");
     saveText.classList.add("clickable");
     saveText.textContent = "Add to Favorites";
+    saveText.onclick = `saveFavorite('${result.url}')`;
     // Card Text
     const cardText = document.createElement("p");
     cardText.textContent = result.explanation;
@@ -71,6 +73,11 @@ async function getNasaPictures() {
   } catch (error) {
     // Catch Error Here
   }
+}
+
+// Add result to favorites
+function saveFavorite(itemUrl) {
+  console.log(itemUrl);
 }
 
 // On load
